3fb5461b8f8a6a437193c13dee444f0b5636e9a3
Crowdbotics <team@crowdbotics.com>
2023-08-09 20:34:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-boat-42753",
+  url: "https://hidden-boat-42753.botics.co",
+  api: "https://hidden-boat-42753.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
